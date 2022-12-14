3f9ad7b3104c743fbccaf965b72f79b218b282fd
Imon <contact@imonweb.co.uk>
2022-12-14 21:50:38 +0000

    rest

--- themes/gymFitness/inc/queries.php
+++ themes/gymFitness/inc/queries.php
@@ -16,6 +16,12 @@ function gymfitness_classes_list() { ?>
       <div class="card-content">
         <a href="<?php the_permalink(); ?>"></a>
         <h3><?php the_title(); ?></h3>
+        <p><?php the_field('class_days'); ?></p>
+        <?php 
+          $start_time = get_field('start_time');
+          $end_time = get_field('end_time');
+        ?>
+        <p><?php echo $start_time . " to " . $end_time ?></p>
       </div>
     </li>
 
