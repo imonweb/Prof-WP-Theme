bc25e6b2d1b5df6c93030f2eb7c8fd4138f908db
Imon <contact@imonweb.co.uk>
2022-12-14 23:32:53 +0000

    single page

--- themes/gymFitness/inc/queries.php
+++ themes/gymFitness/inc/queries.php
@@ -14,14 +14,15 @@ function gymfitness_classes_list() { ?>
     <li class="gym-class card gradient">
       <?php the_post_thumbnail('mediumSize'); ?>
       <div class="card-content">
-        <a href="<?php the_permalink(); ?>"></a>
-        <h3><?php the_title(); ?></h3>
-        <p><?php the_field('class_days'); ?></p>
+        <a href="<?php the_permalink(); ?>">
+          <h3><?php the_title(); ?></h3>
+        </a>
         <?php 
           $start_time = get_field('start_time');
           $end_time = get_field('end_time');
-        ?>
-        <p><?php echo $start_time . " to " . $end_time ?></p>
+          ?>
+        <p><?php ?></p>
+        <p><?php echo the_field('class_days') . " ".  $start_time . " to " . $end_time; ?></p>
       </div>
     </li>
 
--- themes/gymFitness/template-parts/page-loop.php
+++ themes/gymFitness/template-parts/page-loop.php
@@ -6,6 +6,18 @@
     if( has_post_thumbnail() ):
       echo the_post_thumbnail('blog', array('class' => 'featured-image'));
     endif;
+
+    // check the current post type
+    if( get_post_type() === 'gymfitness_classes'):
+     
+          $start_time = get_field('start_time');
+          $end_time = get_field('end_time');
+    ?>
+        <p class="content-class">
+          <?php ?> 
+          <?php echo the_field('class_days') . " ".  $start_time . " to " . $end_time; ?>
+        </p>
+    <?php endif;
   ?>
   <?php //the_post_thumbnail(); ?>
 
