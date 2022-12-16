f2847d277d09d619d2cb50c3727a69d1858a9402
Imon <contact@imonweb.co.uk>
2022-12-16 22:23:29 +0000

    blog entries

--- themes/gymFitness/home.php
+++ themes/gymFitness/home.php
@@ -1,9 +1,30 @@
 <?php get_header(); ?>
 
   <main class="container page section">
-    <?php while(have_posts(  ) ):  the_post(); ?>
-      <h2><?php the_title(); ?></h2>
-    <?php endwhile; ?>
+    <ul class="blog-entries">
+      <?php while(have_posts(  ) ):  the_post(); ?>
+      <li class="card gradient">
+        <?php the_post_thumbnail('mediumSize'); ?>
+          <div class="card-content">
+            <a href="<?php the_permalink(); ?>">
+              <h3><?php the_title(); ?></h3>
+            </a>
+
+            <p class="meta">
+              <span class="text-primary">By: </span>
+              <a href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>">
+                <?php echo get_the_author_meta('display_name'); ?>
+              </a>
+            </p>
+
+            <p class="date-published meta">
+              <span class="text-primary">Date: </span>
+              <?php the_time( get_option( 'date_format' )); ?>
+            </p>
+          </div>
+        </li>
+        <?php endwhile; ?>
+    </ul>
   </main>
 
 <?php get_footer(); ?>
