bd70d22d426e45e31afe8915bbe104d6a613a5fa
Imon <contact@imonweb.co.uk>
2022-12-20 15:46:02 +0000

    categories

---
+++ themes/gymFitness/author.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+  <main class="container page section">
+
+    <?php 
+      $author = get_queried_object();
+      
+    ?>
+    <h2 class="text-center primary-text">Author: <?php echo $author->data->display_name; ?></h2>
+    <p class="text-center">
+      <?php echo get_the_author_meta( 'description', $author->data->ID ); ?>
+    </p>
+   <?php get_template_part('template-parts/blog', 'loop' ); ?>
+  </main>
+
+<?php get_footer(); ?>
--- themes/gymFitness/home.php
+++ themes/gymFitness/home.php
@@ -1,30 +1,7 @@
 <?php get_header(); ?>
 
   <main class="container page section">
-    <ul class="blog-entries">
-      <?php while(have_posts(  ) ):  the_post(); ?>
-      <li class="card gradient">
-        <?php the_post_thumbnail('mediumSize'); ?>
-          <div class="card-content">
-            <a href="<?php the_permalink(); ?>">
-              <h3><?php the_title(); ?></h3>
-            </a>
-
-            <p class="meta">
-              <span class="text-primary">By: </span>
-              <a href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>">
-                <?php echo get_the_author_meta('display_name'); ?>
-              </a>
-            </p>
-
-            <p class="date-published meta">
-              <span class="text-primary">Date: </span>
-              <?php the_time( get_option( 'date_format' )); ?>
-            </p>
-          </div>
-        </li>
-        <?php endwhile; ?>
-    </ul>
+    <?php get_template_part('template-parts/blog', 'loop' ); ?>
   </main>
 
 <?php get_footer(); ?>
