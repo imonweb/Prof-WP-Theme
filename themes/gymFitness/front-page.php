08afe100de91ddefc0f6d4eddd34d923587db987
Imon <contact@imonweb.co.uk>
2022-12-22 12:37:34 +0000

    welcome banner to front-page

--- themes/gymFitness/front-page.php
+++ themes/gymFitness/front-page.php
@@ -1,4 +1,9 @@
 <?php get_header('front'); ?>
 
-
+<?php while(have_posts()): the_post(); ?>
+  <section class="welcome text-center section">
+    <h2 class="text-primary"><?php the_field('welcome_heading'); ?></h2>
+    <p><?php the_field('welcome_text'); ?></p>
+  </section>
+<?php endwhile; ?>
 <?php get_footer(); ?>
